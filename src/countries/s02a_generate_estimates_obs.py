c049fc62f4a801a3ef432972f8687c034a19989e
SallyAJ <sally.jahn@imperial.ac.uk>
2026-04-27 11:40:45 +0100

    harmonize inconsistent CRS assignment

--- src/countries/s02a_generate_estimates_obs.py
+++ src/countries/s02a_generate_estimates_obs.py
@@ -25,33 +25,36 @@ from src.config.data_catalog import (domains_file_subset, get_countries_base, ge
                                      get_country_gadm, get_countries_observation)
 from src.config.data_sets import CHIRPS_CONFIG, ERA5_LAND_CONFIG, SEDAC_CONFIG, get_abbreviation
 from src.utils.path_helper import create_folder, file_storage
-from src.config.param import  crs_reference_global, variables_era5land_list, admin_unit_levels, missing_value
-from src.utils.avg_helper import (admin_name_func, define_names,  clip_pixel_admin_units_levels,
+from src.config.param import crs_reference_global, variables_era5land_list, admin_unit_levels, missing_value
+from src.utils.avg_helper import (admin_name_func, define_names, clip_pixel_admin_units_levels,
                                   weights_cos_lat, weighted_mean, get_domain_from_country)
-from src.utils.pop_helper import prepare_pop, _clip_pop_to_country, get_pop_for_year, get_decade, weights_pop, load_decadal_populations
-from src.utils.data_helper import read_in_xarray_var_crs, read_in_time
+from src.utils.pop_helper import prepare_pop, _clip_pop_to_country, get_pop_for_year, get_decade, weights_pop, \
+    load_decadal_populations
+from src.utils.data_helper import read_in_time
 
 # global parameters
 config = DateUpdateArgs(1981, 2024, 1, 12)
 path_chirps_load = get_compressed_folder(CHIRPS_CONFIG, variable="total_precipitation")
-country_code_sel = "VEN" # select country
+country_code_sel = "VEN"  # select country
 
 
-def main(domain, country_code, period_config, path_chirps, variables_era5land = variables_era5land_list):
+def main(domain, country_code, period_config, path_chirps, variables_era5land=variables_era5land_list):
     shape_country_ad0 = get_country_gadm(country_code, admin_level="ADM_0")
     ds_variables = {}
     var_abb = get_abbreviation("total_precipitation")
-    ds_variables["total_precipitation"] = prepare_continent_file(path_chirps, domain, "total_precipitation", period_config,
-                                       shape_country_ad0, var_abb)
+    ds_variables["total_precipitation"] = prepare_continent_file(path_chirps, domain, "total_precipitation",
+                                                                 period_config,
+                                                                 shape_country_ad0, var_abb)
     for variable in variables_era5land:
         path_era5land_compressed = get_compressed_folder(ERA5_LAND_CONFIG, variable=variable)
         var_abb = get_abbreviation(variable)
-        ds_variables[variable]  = prepare_continent_file(path_era5land_compressed, domain, variable, period_config,
-                                       shape_country_ad0, var_abb)
+        ds_variables[variable] = prepare_continent_file(path_era5land_compressed, domain, variable, period_config,
+                                                        shape_country_ad0, var_abb)
     avg_admin(country_code, shape_country_ad0, ds_variables, variables_era5land, domain, period_config)
 
 
-def prepare_continent_file(path_source, continent, variable, period,  shape_ad0, var_abb, crs=crs_reference_global):
+def prepare_continent_file(path_source, continent, variable, period, shape_ad0, var_abb,
+                           crs_reference=crs_reference_global):
     datasets = []
     for year in period:
         pattern = "{}_{}_{}_compressed.nc".format(continent, variable, year)
@@ -60,7 +63,7 @@ def prepare_continent_file(path_source, continent, variable, period,  shape_ad0,
         datasets.append(ds)
         ds.close()
     ds_merged = xr.concat(datasets, dim="time")
-    ds_merged.rio.write_crs(crs, inplace=True)
+    ds_merged.rio.write_crs(crs_reference, inplace=True)
     clipped_nc_shape_ad0, num_pixels_nc_ad0 = clip_pixel_admin_units_levels(
         ds_merged, shape_ad0, var_abb
     )
@@ -74,14 +77,17 @@ def avg_admin(country_code, shape_country_ad0, ds_variables, variables_era5land,
             admin_names = admin_name_func(shape_country, admin_level=admin_unit)
             indices_list = shape_country.index.values
             for index in indices_list:
-                run_country_obs(country_code, SEDAC_CONFIG, index, shape_country_ad0, shape_country, admin_names, ds_variables,
+                run_country_obs(country_code, SEDAC_CONFIG, index, shape_country_ad0, shape_country, admin_names,
+                                ds_variables,
                                 variables_era5land,
                                 domain,
                                 period_config)
 
-def run_country_obs(country_code, sedac_config, index, shape_country_ad0, shape_country, admin_names, ds_variables, variables_era5land_list,
-                        domain,
-                        period_config):
+
+def run_country_obs(country_code, sedac_config, index, shape_country_ad0, shape_country, admin_names, ds_variables,
+                    variables_era5land_list,
+                    domain,
+                    period_config):
     pop_ssp2_raw = load_decadal_populations(
         sedac_config, scenario="SSP2", start_year=2000, end_year=2100, step=10
     )
@@ -92,17 +98,18 @@ def run_country_obs(country_code, sedac_config, index, shape_country_ad0, shape_
     polygon = shape_country.geometry.iloc[index]
     polygon_gdf = gpd.GeoDataFrame(geometry=[polygon], crs=crs_reference_global)
     cleaned_admin_unit_name = define_names(admin_names[index])
-    pq_file(polygon_gdf= polygon_gdf, cleaned_admin_unit_name=cleaned_admin_unit_name,
-        country=country_code,
-        period=period_config,
-        ds_variables=ds_variables,
-        pop_grids_ssp=POP_SSP2,
-        variables_era5land=variables_era5land_list,
-        reg_sel=domain
-    )
+    pq_file(polygon_gdf=polygon_gdf, cleaned_admin_unit_name=cleaned_admin_unit_name,
+            country=country_code,
+            period=period_config,
+            ds_variables=ds_variables,
+            pop_grids_ssp=POP_SSP2,
+            variables_era5land=variables_era5land_list,
+            reg_sel=domain
+            )
 
 
-def pq_file(polygon_gdf, cleaned_admin_unit_name, country, period, ds_variables, pop_grids_ssp, variables_era5land, reg_sel):
+def pq_file(polygon_gdf, cleaned_admin_unit_name, country, period, ds_variables, pop_grids_ssp, variables_era5land,
+            reg_sel):
     path_country = os.path.join(get_countries_observation(), country)
     storage_path_pq_admin = create_storage_path(
         path_country, reg_sel, cleaned_admin_unit_name,
@@ -111,13 +118,13 @@ def pq_file(polygon_gdf, cleaned_admin_unit_name, country, period, ds_variables,
     )
     # 1) CHIRPS
     results_chirps = obs_avg(ds_variables, polygon_gdf, cleaned_admin_unit_name,
-         pop_grids_ssp,
-        "total_precipitation")
+                             pop_grids_ssp,
+                             "total_precipitation")
     merged_records = {rec["admin_name"]: rec for rec in results_chirps}
     # 2) ERA5_Land variables: add to merged_record
     for variable in variables_era5land:
         results_era5_land = obs_avg(ds_variables, polygon_gdf, cleaned_admin_unit_name,
-            pop_grids_ssp, variable)
+                                    pop_grids_ssp, variable)
         for rec in results_era5_land:
             admin_name = rec["admin_name"]
             if admin_name in merged_records:
@@ -136,8 +143,11 @@ def pq_file(polygon_gdf, cleaned_admin_unit_name, country, period, ds_variables,
 
 def obs_avg(ds_variables, polygon_gdf, cleaned_admin_unit_name, pop_grids, var_long):
     var_name = get_abbreviation(var_long)
-    results=[]
-    mean_values_pop, mean_values_area, mean_values_pop_dynamic, num_pixels_nc = combine_years_obs(var_long, ds_variables, pop_grids, polygon_gdf)
+    results = []
+    mean_values_pop, mean_values_area, mean_values_pop_dynamic, num_pixels_nc = combine_years_obs(var_long,
+                                                                                                  ds_variables,
+                                                                                                  pop_grids,
+                                                                                                  polygon_gdf)
     results.append({
         "admin_name": cleaned_admin_unit_name,
         "pixels": num_pixels_nc,
@@ -187,13 +197,13 @@ def combine_years_obs(variable, ds_variables, pop_grids, polygon_gdf, crs_refere
     return mean_values_pop, mean_values_area, mean_values_pop_dynamic, num_pixels_nc
 
 
-
 # storage
-def create_storage_path(storage_path,continent, gadm_code, data_source, start, end, information, end_format):
+def create_storage_path(storage_path, continent, gadm_code, data_source, start, end, information, end_format):
     create_folder(storage_path)
     storage_path_format = os.path.join(storage_path, "{}_{}_{}_{}_{}_{}.{}".format(continent, gadm_code,
-                                                                             data_source, str(start), str(end), information,
-                                                                             end_format))
+                                                                                   data_source, str(start), str(end),
+                                                                                   information,
+                                                                                   end_format))
     return storage_path_format
 
 
@@ -203,4 +213,5 @@ if __name__ == "__main__":
     period_config_sel = list(range(config.year_start, config.year_end + 1))
     domain_df = pd.read_csv(domains_file_subset, sep="\t")
     domain_sel = get_domain_from_country(domain_df, country_code_sel)
-    main(domain_sel, country_code_sel, period_config_sel, path_chirps=path_chirps_load, variables_era5land=variables_era5land_list)
+    main(domain_sel, country_code_sel, period_config_sel, path_chirps=path_chirps_load,
+         variables_era5land=variables_era5land_list)
